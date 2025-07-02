e0816a999de7bda107ead4d9747e3b1aaa68242a
Garrick Dabbs <71096870+garrickdabbs@users.noreply.github.com>
2025-07-02 04:18:59 -0700

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,4 @@
-# This is the __init__.py file for the SFM-Graph-Service package.
+# This is the __init__.py file for the SFM-Graph-Service core package.
 # It initializes the package and makes its modules importable.
 
 __version__ = "1.0.0"
